411083472c4f1a4f587602ae3cbff7dfe5327629
Sadlineee <sad1ineee@gmail.com>
2024-12-26 22:36:15 +0500

    template

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <link rel='icon' href='./favicon.ico' sizes='any' />
+      </head>
       <body className={oswald.className}>
         <Theme>
           <StyledComponentsRegistry>

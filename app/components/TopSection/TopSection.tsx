1707a609a9fff8431aa34b0fe26266618732d26d
Sadlineee <sad1ineee@gmail.com>
2025-01-06 16:46:11 +0500

    template

--- app/components/TopSection/TopSection.tsx
+++ app/components/TopSection/TopSection.tsx
@@ -4,9 +4,13 @@ export default function TopSection() {
   return (
     <TopSectionBox>
       <TextBox>
-        <PrimaryText>Лучшее решение для бизнеса</PrimaryText>
+        <PrimaryText>
+          Лучшее решение для бизнеса
+        </PrimaryText>
         <hr />
-        <SecondaryText>Мы предлагаем услуги по внедрению инновационных технологий, которые помогут автоматизировать бизнес-процессы и оптимизировать системные решения.</SecondaryText>
+        <SecondaryText>
+          Мы предлагаем услуги по внедрению инновационных технологий, которые помогут автоматизировать бизнес-процессы и оптимизировать системные решения.
+        </SecondaryText>
       </TextBox>
       <Shape />
     </TopSectionBox>
